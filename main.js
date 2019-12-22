9610f28b4c6057a30d483c2fc0a9fd24e3f7636a
aek-fr <59150081+aek-fr@users.noreply.github.com>
2019-12-22 23:38:07 +0100

    Reformated

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 chrome.contextMenus.create({
     title: "Appeler %s",
     contexts:["selection"], 
-onclick: function(selected) {
-	var query = selected.selectionText
-	var query = query.replace(/\s/g, '');
-	if (query.includes('+')) {var query = query.replace(/\(0\)/g,'')};
-	chrome.tabs.create({url: "tel:" + query});
+	onclick: function(selected) {
+		var query = selected.selectionText
+		var query = query.replace(/\s/g, '');
+		if (query.includes('+')) {var query = query.replace(/\(0\)/g,'')};
+		chrome.tabs.create({url: "tel:" + query});
     }
 });
+
