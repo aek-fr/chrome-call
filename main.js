94c47d6458bbd11a96341d13c15d05c45d7f2bdb
aek-fr <59150081+aek-fr@users.noreply.github.com>
2019-12-22 23:19:45 +0100

    0.1 select and right clic to call

---
+++ main.js
@@ -0,0 +1,10 @@
+chrome.contextMenus.create({
+    title: "Appeler %s",
+    contexts:["selection"], 
+onclick: function(selected) {
+	var query = selected.selectionText
+	var query = query.replace(/\s/g, '');
+	if (query.includes('+')) {var query = query.replace(/\(0\)/g,'')};
+	chrome.tabs.create({url: "tel:" + query});
+    }
+});
