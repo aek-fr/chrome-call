b8954232c59704c93c59e90decfbb1d30056b738
aek-fr <59150081+aek-fr@users.noreply.github.com>
2020-01-06 17:05:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ chrome.contextMenus.create({
     title: "Appeler %s",
     contexts:["selection"], 
 	onclick: function(selected) {
-		var query = selected.selectionText
+		var query = selected.selectionText;
 		var query = query.replace(/\s/g, '');
 		if (query.includes('+')) {var query = query.replace(/\(0\)/g,'')};
 		chrome.tabs.create({url: "tel:" + query});
